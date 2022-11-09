18b4770e501c0442362030f5030425f1b606c3a4
Cristiano Nobre <108812671+cristiano-nobre@users.noreply.github.com>
2022-11-09 10:52:10 -0300

    Add files via upload

---
+++ calculo-combustivel-por-km.js
@@ -0,0 +1,9 @@
+const precoCombustivel = 5.79
+const kmPorLitros = 3
+const distanciaViagem = 136
+
+const litrosConsumidos = distanciaViagem / kmPorLitros
+const valorGasto = litrosConsumidos * precoCombustivel
+
+console.log(litrosConsumidos)
+console.log(valorGasto)
